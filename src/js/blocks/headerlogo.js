1d1ebaff6c727370dee5bb19a9fb7dfa4455ea44
Ben Toth Asus LP <zimaben@gmail.com>
2022-03-12 23:10:44 +0800

    Logo Block Tested

--- src/js/blocks/headerlogo.js
+++ src/js/blocks/headerlogo.js
@@ -1,111 +1,85 @@
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
-const { RichText, InspectorControls } = wp.editor; 
-const { PanelBody, PanelRow, ToggleControl, SelectControl, Button, ColorPalette  } = wp.components;
+import kitely_icons from './icons.js';
+
+const { registerBlockType } = wp.blocks; 
+const { MediaUpload, InspectorControls } = wp.blockEditor; 
+const { PanelBody, IconButton, TextControl   } = wp.components;
+
 
 registerBlockType('selfctrl-dashboard/textblock', { 
  
-	title: 'Self CTRL Text Block', // Block title. 
-    icon: 'superhero',  
+	title: 'Kitely Upload Logo', 
+	icon: kitely_icons.kitelytech,
     category: 'kitelytech', 
     //attributes
     attributes: {
-        title: {
+        bgImage: {
             type: 'string',
-            source: 'html',
-            selector: 'h4',
-        }, 
-        body: {
-            type: 'string',
-            source: 'html',
-            selector: 'p'
-        }, 
-        bgColor: {
-            type: 'string'
+			source: 'html',
+			default:null
         },
-        titleColor: {
-            type: 'string'
+        altText: {
+            type: 'string',
+			source: 'html',
+			default:''
         }
     },   
 
 	edit({attributes, setAttributes}){
-		 const {
-            title,
-            body,
-            titleColor,
-            bgColor
+		const { bgImage, altText } = attributes;
 
-        } = attributes;
-        const colors = [
-            { name: 'Black', color: '#000' },
-            { name: 'White', color: '#fff' },
-            { name: 'Self CTRL Blue', color: '#0085ff' },
-        ];
-        function onTitleColorChange(newcolor){
-        	setAttributes({ titleColor: newcolor });
+        function onAltTextChange(newtext){
+        	setAttributes({ altText: newtext });
         }
-        function onChangeTitle(newtitle){
-        	setAttributes({ title: newtitle });
-        }
-        function onChangeBody(newbody){
-        	setAttributes({ body: newbody });
-        }
-        function onBodyColorChange(newcolor){
-        	setAttributes({ bgColor: newcolor });
+        function onSelectImage(newImage){
+        	setAttributes({ bgImage: newImage.url });
         }
 		return ([
 			<InspectorControls style={{marginBottom: '20px' }}>
-				<PanelBody title={'Change Title Color'}>
-				<p><strong>Change Title Color</strong></p>
-				<ColorPalette colors={colors}
-							  value={ titleColor }
-							  onChange={ onTitleColorChange} />
-				<p><strong>Change Background Color</strong></p>
-				<ColorPalette colors={colors}
-							  value={ bgColor }
-							  onChange={ onBodyColorChange} />
-				
+				<PanelBody title="'Upload Logo Image">
+					<p><strong>Upload file (.svg recommended)</strong></p>
+					<MediaUpload
+						onSelect={ onSelectImage }
+						type="images"
+						value={ bgImage }
+						render= {( {open} ) =>{
+							return (<IconButton
+								onClick={ open }
+								icon="upload" 
+								className="editor-media-placeholder__button is-button is-default is-large"
+							>
+								Upload
+							</IconButton>)
+						}}
+					/>
+					<p><strong>Alt Text</strong></p>
+					<TextControl 
+					 value={altText}
+					 onChange={onAltTextChange}
+					 />
 				</PanelBody>
 			</InspectorControls>,
-			<section className="selfctrl-section">
-					<div className="assessment-block" style={{ backgroundColor: bgColor }}>
-
-					<RichText 	key="editable"
-								tagName="h4" 
-								className="block-title"
-								placeholder="Text Block Title" 
-								value={title}
-								onChange={onChangeTitle} 
-								style={{color:titleColor}}
+			<div className="logo">
+				<a href="/">
+					<img className="logo-img"
+						 alt={altText}
+						 src={bgImage}
 					/>
-					<RichText 	key="editable"
-								tagName="p"
-								className="assessment-text"
-								placeholder="Block Body Text"
-								value={body}
-								onChange={onChangeBody} 
-					/>
-				</div>
-			</section>
+				</a>
+			</div>
 		]);
 	},
-
 	save({attributes}) {
-		const {
-            title,
-            body,
-            titleColor,
-            bgColor
+		const { bgImage, altText } = attributes;
 
-        } = attributes;
 		return (
-		<section className="selfctrl-section">
-			<div className="assessment-block" style={{backgroundColor: bgColor}}>
-				<h4 className="block-title" style={{color: titleColor}}>{title}</h4>
-
-				<RichText.Content className="assessment-text" tagName="p" value={body} />
-			</div>
-		</section>
-			
-		);
-	},
+			<div className="logo">
+				<a href="/">
+					<img className="logo-img"
+						alt={altText}
+						src={bgImage}
+					/>
+				</a>
+			</div>	
+		)
+	}
 });
